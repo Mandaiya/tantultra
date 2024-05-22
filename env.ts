f6b1bb5fc254a4d2bda836643cfb8c905c9e5045
natha <108113193+Mandaiya@users.noreply.github.com>
2024-05-22 13:23:34 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,6 +4,6 @@ import { cleanEnv, str } from "envalid";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  BOT_TOKEN: str(),
-  MONGO_URL: str(),
+  6066551482:AAEw8W4KJv7BxRlhI18mwvL6yKpIvJbAZcg: str(),
+  mongodb://clw9dzo3w0027aun26j0we9yi:7hWjvmMAhz2YAsbYnMXloPhM@clw9dzo3y008jn2au26sotiwc:27017/?readPreference=primary&ssl=false: str(),
 });
